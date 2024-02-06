f4f413cc5cf5d2b6a0ca0f05ff631c5d4e2b1b04
Ilya <karpov.ilja@gmail.com>
2024-02-06 17:42:33 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -2,29 +2,36 @@
 # Todo App
 # 
 # Masivi https://mape.gov.lv/catalog/materials/6501426F-B6EC-44B3-8B93-DC553DAE8886/view?preview=7A90D16F-0A8A-4840-A2E3-5EA4F6D4E194
-# Arrays https://www.w3schools.com/python/python_arrays.asp
+# Lists https://www.w3schools.com/python/python_lists.asp
 # 
 
 def add(list, item):
+  # https://www.w3schools.com/python/python_lists_add.asp
   pass
 
 
 def remove(list, index):
+  # https://www.w3schools.com/python/python_lists_remove.asp
   pass
 
 
 def clear(list):
+  # https://www.w3schools.com/python/python_lists_remove.asp
   pass
 
 
 def print_list(list):
+  # https://www.w3schools.com/python/python_lists_loop.asp
   pass
 
+def show(list):
+  # https://www.w3schools.com/python/python_lists_access.asp
+  pass
 
 list = []
 print("List is empty now, what you want to do?")
 while True:
-  choice = int(input("1. Add\n2. Remove\n3. Clear\n4. Print list\n"))
+  choice = int(input("1. Add\n2. Remove\n3. Clear\n4. Print list\n5. Show item by index"))
   if choice == 1:
     item = input("What you want to add?\n")
     add(list, item)
@@ -38,5 +45,7 @@ while True:
     print_list(list)
   elif choice == 4:
     print_list(list)
+  elif choice == 5:
+    show(list)
   else:
     print("Invalid input")
